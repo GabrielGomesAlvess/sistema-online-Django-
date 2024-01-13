840d9a885d28592a5943ef61d746041601de0cb0
unknown <biel.megatech7@gmail.com>
2024-01-12 21:50:22 -0300

    aluno-curso

--- teste/urls.py
+++ teste/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('outro', views.novo, name='novo')
+    path("", views.index, name="index"),
+    path("outro", views.novo, name="novo"),
+    # Aluno
+    path("listar_alunos", views.listarAlunos, name="listar_alunos"),
+    # Curso
+    path("listar_cursos", views.listarCursos, name="listar_cursos"),
 ]
--- teste/views.py
+++ teste/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from teste.models import Aluno, Curso
+
 
 # Create your views here.
 def index(request):
@@ -9,3 +11,13 @@ def index(request):
 
 def novo(request):
     return HttpResponse("Essa é uma requisição")
+
+
+def listarAlunos(request):
+    alunos = Aluno.objects.all()
+    return render(request, "listar_aluno.html", {"alunos": alunos})
+
+
+def listarCursos(request):
+    cursos = Curso.objects.all()
+    return render(request, "listar_curso.html", {"cursos": cursos})
