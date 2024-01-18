8af97255155531c995843194559c78791153522d
unknown <biel.megatech7@gmail.com>
2024-01-18 11:01:40 -0300

    first commit

--- teste/urls.py
+++ teste/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     path("outro", views.novo, name="novo"),
     # Aluno
     path("listar_alunos", views.listarAlunos, name="listar_alunos"),
+    path("incluir_aluno", views.incluirAluno, name="incluir_aluno"),
     # Curso
     path("listar_cursos", views.listarCursos, name="listar_cursos"),
+    path("incluir_curso", views.incluirCurso, name="incluir_curso"),
+    # Editar
+    path("editar_aluno/<int:id>", views.editarAluno, name="editar_aluno"),
+    path("editar_curso/<int:id>", views.editarCurso, name="editar_curso"),
 ]
--- teste/views.py
+++ teste/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from teste.forms import AlunoForm, CursoForm
 
 from teste.models import Aluno, Curso
 
@@ -21,3 +22,51 @@ def listarAlunos(request):
 def listarCursos(request):
     cursos = Curso.objects.all()
     return render(request, "listar_curso.html", {"cursos": cursos})
+
+
+def incluirAluno(request):
+    if request.method == "POST":
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("listar_alunos")
+    else:
+        form = AlunoForm()
+    return render(request, "incluir_aluno.html", {"form": form})
+
+
+def editarAluno(request, id):
+    aluno = Aluno.objects.get(id=id)
+    form = AlunoForm(instance=aluno)
+
+    if request.method == "POST":
+        form = AlunoForm(request.POST, instance=aluno)
+        if form.is_valid():
+            form.save()
+            return redirect("listar_alunos")
+
+    return render(request, "incluir_aluno.html", {"form": form})
+
+
+def incluirCurso(request):
+    if request.method == "POST":
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("listar_cursos")
+    else:
+        form = CursoForm()
+    return render(request, "incluir_curso.html", {"form": form})
+
+
+def editarCurso(request, id):
+    curso = Curso.objects.get(id=id)
+    form = CursoForm(instance=curso)
+
+    if request.method == "POST":
+        form = CursoForm(request.POST, instance=curso)
+        if form.is_valid():
+            form.save()
+            return redirect("listar_cursos")
+
+    return render(request, "incluir_curso.html", {"form": form})
